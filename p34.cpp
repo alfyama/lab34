d563d1706b4209afddb8749bfa22ecb0f00d80d4
Jonas Kuß <jonaskuss@protonmail.com>
2023-02-08 11:10:23 +0100

    Simple initial implementation of Fixing Pass

--- p34.cpp
+++ p34.cpp
@@ -67,8 +67,41 @@ public:
   FixingPass() : FunctionPass(ID) {}
 
   virtual bool runOnFunction(Function &F) {
-    // TODO
-    errs() << "fix-pass\n";
+    std::set<Value *> definedVariables;
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (auto *LI = dyn_cast<LoadInst>(&I)) {
+          Value *ptrOperand = LI->getPointerOperand();
+          if (definedVariables.count(ptrOperand) == 0) {
+            llvm::StoreInst *SI;
+
+            auto *type = LI->getType();
+            if (type->isIntegerTy()) {
+              Value *init_int = ConstantInt::get(type, 10, true);
+              SI = new StoreInst(init_int, ptrOperand, LI);
+            } else if (type->isFloatTy()) {
+              Value *init_float = ConstantFP::get(type, 20.0);
+              SI = new StoreInst(init_float, ptrOperand, LI);
+            } else if (type->isDoubleTy()) {
+              Value *init_double = ConstantFP::get(type, 30.0);
+              SI = new StoreInst(init_double, ptrOperand, LI);
+            }
+
+            // NOTE: Can there be a problem with inserting an instruction while
+            // looping over them?
+            SI->insertBefore(LI);
+            definedVariables.insert(ptrOperand);
+          }
+        } else if (auto *SI = dyn_cast<StoreInst>(&I)) {
+          // If the instruction is a store instruction, add the stored variable
+          // to the set of defined variables
+          definedVariables.insert(SI->getPointerOperand());
+        }
+      }
+    }
+
+    // FIXME: should only return true if the IR was modified, i.e. there were
+    // uses of uninitialized variables
     return true;
   }
 };
