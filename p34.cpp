20d2643a57db4eb0707dbc75cf7f34a62c1ee3fb
Jonas Kuß <jonaskuss@protonmail.com>
2023-02-08 15:34:17 +0100

    LLVM Fix Pass: Find correct insertion point
    Before, missing Store Instructions would be inserted
    just before the first load. This breaks in cases, where
    the load is nested inside loops, because the value of the
    variable would then be reset in each iteration.
    
    Now, we find the first Alloca Instruction (declaration) for
    the variable and insert the Store Instruction after that.
    There may be some problems with multiple Alloca Instructions
    for a single variable, but I don't we will ever have to deal with it.
    
    This commit makes all the IR Fix tests pass!

--- p34.cpp
+++ p34.cpp
@@ -67,31 +67,50 @@ public:
   FixingPass() : FunctionPass(ID) {}
 
   virtual bool runOnFunction(Function &F) {
+    // NOTE: we maybe don't even need this because of uses() and users()
+    // methods, but for now it is fine.
     std::set<Value *> definedVariables;
+
     for (auto &BB : F) {
       for (auto &I : BB) {
-        if (auto *LI = dyn_cast<LoadInst>(&I)) {
+        if (LoadInst *LI = dyn_cast<LoadInst>(&I)) {
           Value *ptrOperand = LI->getPointerOperand();
           if (definedVariables.count(ptrOperand) == 0) {
-            llvm::StoreInst *SI;
+            StoreInst *SI;
 
-            auto *type = LI->getType();
+            Instruction *marker = nullptr;
+            for (Use &U : ptrOperand->uses()) {
+              if (auto AI = dyn_cast<AllocaInst>(U)) {
+                // NOTE: The StoreInst constructor forces us to pass an
+                // insertBefore parameter. Because of this, we need this kinda
+                // awkward logic here.
+                marker = AI->getNextNonDebugInstruction();
+                break;
+              }
+            }
+
+            if (marker == nullptr) {
+              errs() << "No declaration found for variable "
+                     << ptrOperand->getName()
+                     << ". Not our problem, crashing ¯\\_(ツ)_/¯\n";
+              exit(EXIT_FAILURE);
+            }
+
+            Type *type = LI->getType();
             if (type->isIntegerTy()) {
               Value *init_int = ConstantInt::get(type, 10, true);
-              SI = new StoreInst(init_int, ptrOperand, LI);
+              SI = new StoreInst(init_int, ptrOperand, marker);
             } else if (type->isFloatTy()) {
               Value *init_float = ConstantFP::get(type, 20.0);
-              SI = new StoreInst(init_float, ptrOperand, LI);
+              SI = new StoreInst(init_float, ptrOperand, marker);
             } else if (type->isDoubleTy()) {
               Value *init_double = ConstantFP::get(type, 30.0);
-              SI = new StoreInst(init_double, ptrOperand, LI);
+              SI = new StoreInst(init_double, ptrOperand, marker);
             }
 
             definedVariables.insert(ptrOperand);
           }
-        } else if (auto *SI = dyn_cast<StoreInst>(&I)) {
-          // If the instruction is a store instruction, add the stored variable
-          // to the set of defined variables
+        } else if (StoreInst *SI = dyn_cast<StoreInst>(&I)) {
           definedVariables.insert(SI->getPointerOperand());
         }
       }
